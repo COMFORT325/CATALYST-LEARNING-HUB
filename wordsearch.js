1325ecfc10549cc9c64aad16e29805203d3e377d
COMFORT325 <tinnythembi00@gmail.com>
2025-10-16 01:50:40 +0200

    Add files via upload

---
+++ wordsearch.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+      <h1 className='text-3xl font-bold mb-4'>Life Sciences Word Search</h1>
+      <p className='text-gray-700'>Interactive word search coming soon...</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
